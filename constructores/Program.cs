a42de0d7e5ee9acc39c7de80993364528fbda79a
Alexandro <alejandro.gh98@outlook.com>
2024-06-18 16:37:05 -0600

    final parcial oop

--- constructores/Program.cs
+++ constructores/Program.cs
@@ -10,8 +10,10 @@ namespace UsoCoches {
             Car car01 = new Car(); // Crear instancia de tipo Car y darle un estado inicial al coche (con ayuda del constructor y lo definido dentro de él.)
             Console.WriteLine(car01.getInfoCar());
 
-            Car car02 = new Car(3600, 2000);
+            Car car02 = new Car(3600, 2000, "Vinipiel"); // Instancia con constructor sobrecargado
             Console.WriteLine(car02.getInfoCar());
+            car02.setExtras(true, "Cuero");
+            Console.WriteLine(car02.getExtras());
         }
     }
 
@@ -22,13 +24,15 @@ namespace UsoCoches {
             numWheels = 4;
             lenght = 2300.5;
             width = 1800.0;
+            typeTapestry = "Tela";
         }
 
         // Constructor sobrecargado para definir propiedades diferentes.
-        public Car(double lenghtCar, double widthCar) {
+        public Car(double lenghtCar, double widthCar, string typeCover) {
             numWheels = 4;
             lenght = lenghtCar;
             width = widthCar;
+            typeTapestry = typeCover;
         }
 
         // Propiedades
@@ -40,9 +44,20 @@ namespace UsoCoches {
 
         // Metodos
 
+        // Metodo getting
         public String getInfoCar() { // Metodos getting para acceder a información encapsulada y que no se pueda modificar
             // Por convención estos metodos inician con get.
-            return $"Información del coche:\n Ruedas: {numWheels} Largo: {lenght} Ancho: {width}";
+            return $"Información del coche:\n Ruedas: {numWheels} Largo: {lenght}, Ancho: {width}, Tapicería: {typeTapestry}";
+        }
+
+        public string getExtras() {
+            return $"Información extra:\n Aire acondicionado: {airConditioner}, Tapicería: {typeTapestry}";
+        }
+
+        // Metodo setting
+        public void setExtras(bool hasAir, string typeTape) { // Ningun setter devuelve algo, al revés, su proposito es añadir
+            airConditioner = hasAir;
+            typeTapestry = typeTape;
         }
     }
 }
--- constructores/obj/Debug/net8.0/constructores.AssemblyInfo.cs
+++ constructores/obj/Debug/net8.0/constructores.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("constructores")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dfe68e1c7269a148b59d76f68c7a2a80562f153")]
 [assembly: System.Reflection.AssemblyProductAttribute("constructores")]
 [assembly: System.Reflection.AssemblyTitleAttribute("constructores")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
